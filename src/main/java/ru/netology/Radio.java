cb11f49abd141949cd2385bfe5bebad9e16b3f69
ARyabtsev79 <ryabtsev79@gmail.com>
2021-06-20 16:21:09 +0300

    update flex new code

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,81 +1,97 @@
 package ru.netology;
 
 public class Radio {
-    private int currentRadioStation;
-    private int minRadioStation = 0;
-    private int maxRadioStation;
-    private int volume;
     private int minVolume = 0;
-    private int maxVolume;
+    private int maxVolume = 100;
+    private int currentVolume;
+    private final int minRadioStation = 0;
+    private int maxRadioStation = 10;
+    private int currentRadioStation;
+    private boolean on;
 
-    public Radio(int currentRadioStation, int maxRadioStation, int volume, int maxVolume) {
-        this.currentRadioStation = currentRadioStation;
-        this.maxRadioStation = maxRadioStation;
-        this.volume = volume;
+    public Radio (int minVolume, int maxVolume, int currentVolume) {
+        this.minVolume = minVolume;
         this.maxVolume = maxVolume;
+        this.currentVolume = currentVolume;
+    }
+
+    public Radio(int maxRadioStation, int currentRadioStation) {
+        this.maxRadioStation = maxRadioStation;
+        this.currentRadioStation = currentRadioStation;
     }
 
 
+    public Radio() {
+    }
 
+    public int getMinVolume() {
+
+        return minVolume;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > maxVolume) {
+            this.currentVolume = maxVolume;
+            return;
+        }
+        if (currentVolume < minVolume) {
+            this.currentVolume = minVolume;
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
 
     public int getCurrentRadioStation() {
         return currentRadioStation;
     }
+    public void increaseVolume() {
+        if (currentVolume >= maxVolume) {
+            return;
+        }
+        this.currentVolume++;
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume <= minVolume) {
+            return;
+        }
+        this.currentVolume--;
+
+    }
+
+    public void nextRadioStation() {
+        if (currentRadioStation < maxRadioStation) {
+            currentRadioStation++;
+        } else {
+            currentRadioStation = minRadioStation;
+        }
+    }
+
+    public void prevRadioStation() {
+        if (currentRadioStation > minRadioStation) {
+            currentRadioStation--;
+        } else {
+            currentRadioStation = maxRadioStation;
+        }
+    }
 
     public void setCurrentRadioStation(int currentRadioStation) {
         if (currentRadioStation > maxRadioStation) {
+            this.currentRadioStation = maxRadioStation;
             return;
         }
         if (currentRadioStation < minRadioStation) {
+            this.currentRadioStation = minRadioStation;
             return;
         }
         this.currentRadioStation = currentRadioStation;
     }
-
-
-    public void changeOnPreviousRadioStation() {
-        if (currentRadioStation == minRadioStation) {
-            this.currentRadioStation = maxRadioStation;
-            return;
-        }
-        this.currentRadioStation--;
-    }
-
-
-    public void changeOnNextRadioStation() {
-        if (currentRadioStation == maxRadioStation) {
-            this.currentRadioStation = minRadioStation;
-            return;
-        }
-        this.currentRadioStation++;
-    }
-
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        if (volume < minVolume) {
-            return;
-        }
-        if (volume > maxVolume) {
-            return;
-        }
-        this.volume = volume;
-    }
-
-    public void volumeUpForOne() {
-
-        if (volume < maxVolume) {
-            this.volume++;
-        }
-    }
-
-    public void volumeDownForOne() {
-        if (volume > minVolume) {
-            this.volume--;
-        }
-    }
-
 }
