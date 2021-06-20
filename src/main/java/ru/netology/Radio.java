e73072c2bc220a60cf1b96595924ef8c3ae2c6ae
ARyabtsev79 <ryabtsev79@gmail.com>
2021-06-20 15:05:00 +0300

    flex update errors

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -2,11 +2,11 @@ package ru.netology;
 
 public class Radio {
     private int currentRadioStation;
-    private final int minRadioStation = 0;
-    private final int maxRadioStation;
+    private int minRadioStation = 0;
+    private int maxRadioStation;
     private int volume;
-    private final int minVolume = 0;
-    private final int maxVolume;
+    private int minVolume = 0;
+    private int maxVolume;
 
     public Radio(int currentRadioStation, int maxRadioStation, int volume, int maxVolume) {
         this.currentRadioStation = currentRadioStation;
